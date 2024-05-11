32495f9b9cbccfafbf5444779ba4d1e20c66393f
vaibhavdikha2206 <vaibhavdikha@gmail.com>
2024-05-12 02:51:57 +0530

    Test for CICD

--- src/main/java/com/simplified/interconnected/controllers/Test.java
+++ src/main/java/com/simplified/interconnected/controllers/Test.java
@@ -14,7 +14,7 @@ public class Test {
     @GetMapping("/test2")
     public String getString() throws IOException {
         System.out.println("sending response");
-        return "Test App";
+        return "Test App New";
     }
 
 }
